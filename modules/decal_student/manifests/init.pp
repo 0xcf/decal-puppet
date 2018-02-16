1ebf348d0b14cd0b6d082801c3dbc93e8475ab68
Jason Perrin <jvperrin@ocf.berkeley.edu>
2018-02-15 19:21:57 -0800

    Remove .pp extension on includes

--- modules/decal_student/manifests/init.pp
+++ modules/decal_student/manifests/init.pp
@@ -1,12 +1,12 @@
 class decal_student {
-  include decal_student::labs::lab1.pp
-  include decal_student::labs::lab2.pp
-  include decal_student::labs::lab3.pp
-  include decal_student::labs::lab4.pp
-  include decal_student::labs::lab5.pp
-  include decal_student::labs::lab6.pp
-  include decal_student::labs::lab7.pp
-  include decal_student::labs::lab8.pp
-  include decal_student::labs::lab9.pp
-  include decal_student::labs::lab10.pp
+  include decal_student::labs::lab1
+  include decal_student::labs::lab2
+  include decal_student::labs::lab3
+  include decal_student::labs::lab4
+  include decal_student::labs::lab5
+  include decal_student::labs::lab6
+  include decal_student::labs::lab7
+  include decal_student::labs::lab8
+  include decal_student::labs::lab9
+  include decal_student::labs::lab10
 }
