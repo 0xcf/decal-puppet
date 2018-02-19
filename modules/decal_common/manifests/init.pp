df02efde467ff5c399b1c6c26ab91bd72d6e8cb1
Jason Perrin <jvperrin@ocf.berkeley.edu>
2018-02-18 18:58:51 -0800

    Add common resolvconf networking config

--- modules/decal_common/manifests/init.pp
+++ modules/decal_common/manifests/init.pp
@@ -9,6 +9,7 @@ class decal_common {
      'fail2ban',
      'htop',
      'iotop',
+     'resolvconf',
      'tree',
      'virtualenv',
      'git',
@@ -30,5 +31,20 @@ class decal_common {
     command     => 'puppet agent -t',
     user        => 'root',
     minute      => [fqdn_rand(10), fqdn_rand(10) + 10, fqdn_rand(10) + 20, fqdn_rand(10) + 30, fqdn_rand(10) + 40, fqdn_rand(10) + 50],
+  }
+
+  # Don't run puppet as a service since it is being run as a cron job
+  service { 'puppet':
+    ensure => stopped,
+    enable => false,
+  }
+
+  # Set up networking to allow puppet to find the puppet master
+  #
+  # Note: configuring this with puppet doesn't make much sense since puppet
+  # can't run if this doesn't work already, but at least for anyone reading
+  # this it will make it clearer what needs to be done to get puppet working.
+  file { '/etc/resolvconf/resolv.conf.d/base'
+    content => "domain decal.xcf.sh\n",
   }
 }
